de397f0bc966a059c912984933472383a4b36158
John Doe <johndoe@email.com>
2024-09-12 19:16:14 -0400

    Initialize inventory with product objects

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    { name: "Guitar", price: 250, quantity: 90, lowStockLevel: 10 },
+    { name: "Piano", price: 2400, quantity: 20, lowStockLevel: 5 },
+    { name: "Drum Set", price: 800, quantity: 15, lowStockLevel: 3 },
+    { name: "Violin", price: 1000, quantity: 100, lowStockLevel: 10 },
+    { name: "Saxophone", price: 750, quantity: 40, lowStockLevel: 8 }
+];
