8efcb1b71dd94b61e6ebc30d648a8c0f3fc207d5
John Doe <johndoe@email.com>
2024-09-12 21:25:09 -0400

    Create displayProductDetails function

--- cc_4.js
+++ cc_4.js
@@ -7,3 +7,20 @@ const inventory = [
     { name: "Violin", price: 1000, quantity: 100, lowStockLevel: 10 },
     { name: "Saxophone", price: 750, quantity: 40, lowStockLevel: 8 }
 ];
+
+
+
+//Task 2: Create a Function to Display Product Details
+
+function displayProductDetails(i) {
+    console.log(`Instrument's name: ${i.name}`);
+    console.log(`Price: $${i.price}`);
+    console.log(`Quantity in stock: ${i.quantity}`);
+    // checking the status of the product's stock. If true then Low Stock, if false then In Stock
+    console.log(`Stock status: ${i.quantity <= i.lowStockLevel ? "Low Stock, Order More!" : "In Stock"}`);
+};
+
+// displaying the function results for each product in the inventory array
+console.log("Product Information List:");
+
+inventory.forEach(displayProductDetails);
